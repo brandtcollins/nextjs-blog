321c73274a83de4dc2226de50f7d92191d1c744c
Work <seebrandt@gmail.com>
2022-05-03 21:05:22 -0400

    Add head to projects page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,9 +1,26 @@
 import { FunctionComponent } from "react";
+import Head from "next/head";
 
 interface ProjectsPageProps {}
 
 const ProjectsPage: FunctionComponent<ProjectsPageProps> = () => {
-  return <div>This is the project page</div>;
+  return (
+    <>
+      <Head>
+        <title>Brandt Collins - Designer and Developer</title>
+        <meta
+          property="og:title"
+          content="Brandt Collins - Designer and Developer"
+          key="title"
+        />
+        <meta
+          name="description"
+          content="Front end web developer and UX/UI designer"
+        ></meta>
+      </Head>
+      <div>This is the project page</div>
+    </>
+  );
 };
 
 export default ProjectsPage;
